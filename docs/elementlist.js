2d3404b938c78a87c035e15f68216e05449ada49
Heverton Coneglian de Freitas <hevertonfreitas1@yahoo.com.br>
2016-01-31 21:13:41 -0200

    Documentação atualizada

--- docs/elementlist.js
+++ docs/elementlist.js
@@ -1,3 +1,3 @@
 
 var ApiGen = ApiGen || {};
-ApiGen.elements = [["c","Exception"],["c","Hevertonfreitas\\Bulario\\Bula"],["c","Hevertonfreitas\\Bulario\\Bulario"],["c","Hevertonfreitas\\Bulario\\BularioTest"],["c","Hevertonfreitas\\Bulario\\BulaTest"],["c","Hevertonfreitas\\Bulario\\DadosBula"]];
+ApiGen.elements = [["c","Exception"],["c","Hevertonfreitas\\Bulario\\Bula"],["c","Hevertonfreitas\\Bulario\\Bulario"],["c","Hevertonfreitas\\Bulario\\BularioTest"],["c","Hevertonfreitas\\Bulario\\BulaTest"],["c","Hevertonfreitas\\Bulario\\DadosBula"],["c","JsonSerializable"]];
